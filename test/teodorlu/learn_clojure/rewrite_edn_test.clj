a5bd5170522245d8041e58030e41fb0a5417ad38
Teodor Heggelund <git@teod.eu>
2023-09-24 16:43:48 +0200

    Call `str` to get string

--- test/teodorlu/learn_clojure/rewrite_edn_test.clj
+++ test/teodorlu/learn_clojure/rewrite_edn_test.clj
@@ -1,6 +1,7 @@
 (ns teodorlu.learn-clojure.rewrite-edn-test
   (:require
    [borkdude.rewrite-edn :as r]
+   [clojure.edn :as edn]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest a
@@ -8,3 +9,10 @@
     (is (r/parse-string "{:deps {}}")))
   (testing "parsing ill-formed EDN throws"
     (is (thrown? clojure.lang.ExceptionInfo (r/parse-string "{:deps {}")))))
+
+(deftest b
+  (let [my-deps (-> (r/parse-string "{}")
+                    (r/assoc-in [:deps 'borkdude/rewrite-edn] {:mvn/version "0.4.7"}))]
+    (is my-deps)
+    (is (contains? (edn/read-string (str my-deps))
+                   :deps))))
