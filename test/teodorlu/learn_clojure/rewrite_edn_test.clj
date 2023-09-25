810fbd38a12ae84537266d9d154ca1c2476e227e
Teodor Heggelund <git@teod.eu>
2023-09-25 10:16:38 +0200

    Delete code from tests that prints to console

--- test/teodorlu/learn_clojure/rewrite_edn_test.clj
+++ test/teodorlu/learn_clojure/rewrite_edn_test.clj
@@ -30,37 +30,6 @@
 {:deps
  {hiccup {:mvn/version \"1.0.4\"}}}
 "]
-  (-> deps-str
-      r/parse-string
-      (r/assoc-in [:aliases :neil :name] 'mylib/mylib)
-      str
-      println))
-
-;; how to I add whitespace with rewrite-edn?
-
-(let [deps-str "
-{:deps
- {hiccup {:mvn/version \"1.0.4\"}}}
-"]
-  (-> deps-str
-      r/parse-string
-      (r/assoc :aliases {})
-      str
-      println))
-
-(let [deps-str "
-{:deps
- {hiccup {:mvn/version \"1.0.4\"}}}"]
-  (-> deps-str
-      r/parse-string
-      (r/assoc-in [:aliases :neil] {})
-      str
-      println))
-
-(let [deps-str "
-{:deps
- {hiccup {:mvn/version \"1.0.4\"}}}
-"]
   (= "
 {:deps
  {hiccup {:mvn/version \"1.0.4\"}}
